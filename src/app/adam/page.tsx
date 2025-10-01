a2df383a375abb97e46f932922d63492700bbadf
logesh2496 <logesh.r24@gmail.com>
2025-10-01 17:26:57 +0530

    fix: add navbar

--- src/app/adam/page.tsx
+++ src/app/adam/page.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import MindshareLeaderboard from "@/components/mindshare-leaderboard";
+import Navbar from "@/components/navbar";
 
 export default function Page() {
   return (
-    <MindshareLeaderboard
-      title="$ADAM"
-      moto="The Rise of Adam"
-      projectId="adam_songjam"
-      timeframes={["ALL"]}
-      backgroundImageUrl="/images/banners/songjam.png"
-    />
+    <div className="relative bg-cover bg-top p-4 min-h-screen md:min-h-auto md:pb-[200px]">
+      <Navbar />
+      <MindshareLeaderboard
+        title="$ADAM"
+        moto="The Rise of Adam"
+        projectId="adam_songjam"
+        timeframes={["ALL"]}
+        backgroundImageUrl="/images/banners/songjam.png"
+      />
+    </div>
   );
 }
